e14c2a5d8697477081a29c130c3204461fdc1a9e
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-02-24 12:58:10 -0300

    definiendo rutas

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const path = require('path');
+const path = require('path');/* me une directorios */
+const multer = require('multer'); 
 // iniclizamos y exjecutamos express, tal vez luego lo ejecuto en una bd
 
 const app = express();
 
 
 app.set('port', 3000); 
-app.set('port', 3000); 
+app.set('views', path.join(__dirname, 'views')); /*  me une dos directorios, lo que hace es decir donde esta la carpeta views */
 app.set('view engine', 'ejs');
 
 /* Routes */
 app.get('/', (req, res) => {
-    res.render();
-})
+    res.render('index');/* formulario o create  */
+});
+/* es lo qu em envia lo que hace el formulario */
+app.post('/upload', (req, res) => {
+    res.send('upload');
+});
 
 /* iniciamos el servidor */
 
